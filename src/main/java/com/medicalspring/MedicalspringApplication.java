7e4a1ee14806e3a73679b2ae001ef9d86cc5b855
bdziedzic <bartosz.dziedzic94@gmail.com>
2019-05-09 14:34:06 +0200

    first commit

---
+++ src/main/java/com/medicalspring/MedicalspringApplication.java
@@ -0,0 +1,13 @@
+package com.medicalspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalspringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/medicalspring/MedicalspringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.medicalspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MedicalspringApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
