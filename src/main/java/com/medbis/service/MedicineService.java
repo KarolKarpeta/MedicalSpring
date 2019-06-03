d5b8850d7970403c8cf9624eb73e0a8ab258e5b7
Los4U <Backup_2018>
2019-06-03 15:00:00 +0200

    add medicineService interface, medicineServiceImp, MedicineRepository

---
+++ src/main/java/com/medbis/service/MedicineService.java
@@ -0,0 +1,18 @@
+package com.medbis.service;
+
+
+import com.medbis.entity.Medicine;
+
+import java.util.List;
+
+public interface MedicineService {
+
+    public List<Medicine> findAll();
+
+    public Medicine findById(int theId);
+
+    public void save(Medicine theMedicine);
+
+    public void deleteById(int theId);
+
+}
---
+++ src/main/java/com/medbis/service/MedicineServiceImpl.java
@@ -0,0 +1,48 @@
+package com.medbis.service;
+
+import com.medbis.entity.Medicine;
+import com.medbis.repository.MedicineRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MedicineServiceImpl implements MedicineService {
+
+    private MedicineRepository medicineRepository;
+
+    @Autowired
+    public MedicineServiceImpl(MedicineRepository medicineRepository) {
+        this.medicineRepository = medicineRepository;
+    }
+
+    @Override
+    public List<Medicine> findAll() {
+        System.out.println("medicineRepository");
+        return medicineRepository.findAll();
+    }
+
+    @Override
+    public Medicine findById(int theId) {
+        Optional<Medicine> result = medicineRepository.findById(theId);
+        Medicine theMedicine = null;
+        if(result.isPresent()){
+            theMedicine = result.get();
+        }else{
+            throw new RuntimeException("Dod not find medicine id" + theId);
+        }
+        return theMedicine;
+    }
+
+    @Override
+    public void save(Medicine theMedicine) {
+        medicineRepository.save(theMedicine);
+    }
+
+    @Override
+    public void deleteById(int theId) {
+        medicineRepository.deleteById(theId);
+    }
+}
