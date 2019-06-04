d0ec45e12067e81914887f36c4c1b5457958aeef
Los4U <Backup_2018>
2019-06-04 19:05:35 +0200

    rearange folders, create 2 separete folders, 1 interfaces, 1 implementation

--- src/main/java/com/medbis/controller/MedicineController.java
+++ src/main/java/com/medbis/controller/MedicineController.java
@@ -2,7 +2,7 @@ package com.medbis.controller;
 
 
 import com.medbis.entity.Medicine;
-import com.medbis.service.MedicineService;
+import com.medbis.service.interfaces.MedicineService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
