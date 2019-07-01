9b0e5bc723e47546592b10e8918417a876162fde
Los4U <Backup_2018>
2019-07-01 03:13:53 +0200

    continue adding new visit, with employeeID and patientID, and date, status

--- src/main/java/com/medbis/controller/VisitController.java
+++ src/main/java/com/medbis/controller/VisitController.java
@@ -1,12 +1,18 @@
 package com.medbis.controller;
 
 import com.medbis.entity.Treatment;
+import com.medbis.entity.Visit;
 import com.medbis.service.interfaces.VisitService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
 
 @Controller
 public class VisitController {
@@ -31,7 +37,23 @@ public class VisitController {
     @GetMapping("/visits/showFormForAddVisit")
     public String showFormForAddVisit(@RequestParam("patientId")int thePatientId, Model theModel){
         //Patient newPatient = (Patient) userFactory.getNewUser("patient");
+        Visit newVisit = new Visit();
+        theModel.addAttribute("visit", newVisit);
+        theModel.addAttribute("patientId", thePatientId);
         return "visits/visit-form";
+    }
+
+    //ADDING NEW VISITS
+    @PostMapping("/visits/addNewNewVisit")
+    public String addNewMedicine(@Valid @ModelAttribute("visit") Visit theVisit,
+                                 BindingResult bindingResult){
+        if (bindingResult.hasErrors()){
+            return "medicine/visit-form";
+        }else{
+            System.out.println("Visit to save:" + theVisit.toString());
+            visitService.save(theVisit);
+            return "redirect:/visits";
+        }
     }
 
 //    @GetMapping ("/patients/showPatientDetails")
