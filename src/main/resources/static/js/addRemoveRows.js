e0ea1d90d77124662bb6dbc7a87595b7da4c161f
Los4U <inz.kamil.wos@gmail.com>
2019-10-08 23:50:25 +0200

    add 3 more function for adding and deleting diseases, repleacing the html code

--- src/main/resources/static/js/addRemoveRows.js
+++ src/main/resources/static/js/addRemoveRows.js
@@ -1,9 +1,10 @@
-function replaceTable(html) {
+function replaceTableMedicine(html) {
     // Replace the <fieldset id="medicineTable"> with a new one returned by server.
     $('#medicineTableF').replaceWith($(html));
+
+    // Material Select Initialization
+    $('.selectMedicine').materialSelect();
 }
-
-
 function addRowMedicine() {
         // e.preventDefault();
         var data = $('form').serialize();
@@ -11,17 +12,47 @@ function addRowMedicine() {
         // POSTed only when clicked. Since "event.preventDefault();" prevents from
         // actual clicking the button, following line will add parameter to form
         // data.
-        data += '&addRow=addRow';
-        $.post('/patients/addNewPatient', data, replaceTable);
+        data += '&addMedicineRow=addMedicineRow';
+        $.post('/patients/addNewPatient', data, replaceTableMedicine);
     }
 
 function dellRowMedicine(e) {
         // event.preventDefault();
         var data = $('form').serialize();
         // Add parameter and index of item that is going to be removed.
-        data += '&removeRow=' + e.target.value;
-        $.post('/patients/addNewPatient', data, replaceTable);
+        data += '&removeMedicineRow=' + e.target.value;
+        $.post('/patients/addNewPatient', data, replaceTableMedicine);
     }
+
+
+function replaceTableDisease(html) {
+    // Replace the <fieldset id="medicineTable"> with a new one returned by server.
+    $('#diseaseTableF').replaceWith($(html));
+
+    // Material Select Initialization
+    $('.selectDisease').materialSelect();
+}
+
+
+function addRowDisease() {
+    // e.preventDefault();
+    var data = $('form').serialize();
+    // Add parameter "addItem" to POSTed form data. Button's name and value is
+    // POSTed only when clicked. Since "event.preventDefault();" prevents from
+    // actual clicking the button, following line will add parameter to form
+    // data.
+    data += '&addDiseaseRow=addDiseaseRow';
+    $.post('/patients/addNewPatient', data, replaceTableDisease);
+}
+
+function dellRowDisease(e) {
+    // event.preventDefault();
+    var data = $('form').serialize();
+    // Add parameter and index of item that is going to be removed.
+    data += '&removeDiseaseRow=' + e.target.value;
+    $.post('/patients/addNewPatient', data, replaceTableDisease);
+}
+
 
 
 // function addRowMedicine() {
